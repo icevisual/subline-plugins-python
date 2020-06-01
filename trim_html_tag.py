f8216007a0d22abc6f3d1ecdfcf62608b557866d
icevisual <icevisual@hotmail.com>
2020-06-01 20:44:58 +0800

    f

--- trim_html_tag.py
+++ trim_html_tag.py
@@ -1,6 +1,43 @@
 import sublime, sublime_plugin
 
 class TrimHtmlTagCommand(sublime_plugin.TextCommand):
+
+  @staticmethod
+  def number2chinese(num):
+    names = '零一二三四五六七八九十百千万'
+    result = ''
+    if num <= 10:
+      return names[num:num+1]
+    if num < 100:
+      shi = int(num / 10)
+      ge = num % 10
+      result = names[shi:shi+1] + names[10:11]
+      if ge > 0:
+        result += names[ge:ge+1]
+      return result
+    if num < 1000:
+      bai = int(num / 100)
+      shi = int((num - bai * 100)/ 10)
+      ge = num % 10
+      result = names[bai:bai+1] + names[11:12]
+      if shi > 0:
+        result += names[shi:shi+1]+ names[10:11]
+      elif ge != 0:
+        result += names[0:0+1]
+      if ge > 0:
+        result += names[ge:ge+1]
+      return result
+    return result
+
+  def print_sql(self):
+    print(TrimHtmlTagCommand.number2chinese(100))
+    # ('N000000000020A', 'G20', '通用 二十', '2', '0', ':108:', '1492076318')
+    for i in range(21,101):
+      print("('N000000000%03dA', 'G%d', '通用 %s', '2', '0', ':108:', '1492076318')," % (i, i, TrimHtmlTagCommand.number2chinese(i)))
+    a = 2
+    if a is 2:
+        return
+
   def run(self, edit):
     pattern = r'<(?:[^"\'>]|(["\'])[^"\']*\1)*>'
     # pattern = r'[\n\s]+'
